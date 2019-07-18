ac64e16e05c5ad1df0bf28917fa5d8a8000e0579
Sokhan Sanj David <david.sokhan-sanj@epitech.eu>
2019-07-18 14:26:02 +0200

    implement chat with test command fonctional without sql database

---
+++ chatbot/lib/main.dart
@@ -0,0 +1,8 @@
+import 'package:flutter/material.dart';
+import 'front/landing_pages.dart';
+
+void main()  { // it's a method
+  runApp(new MaterialApp(
+    home: new MyApp(),
+  ));
+}
--- skydoo_communication_app/lib/pages/main_page.dart
+++ skydoo_communication_app/lib/pages/main_page.dart
@@ -1,30 +1,30 @@
-import 'package:flutter/material.dart';
-import './show_page.dart';
+// import 'package:flutter/material.dart';
+// import './show_page.dart';
 
-class MainPage extends StatelessWidget {
-  Widget build (BuildContext context) {
-    return new Material(
-      color: Colors.white24,
-      child: new InkWell(
-        onTap: () => Navigator.of(context).push(new MaterialPageRoute(builder: (BuildContext context) => ShowPage())),
-        child: new Column(
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: <Widget>[
-            new Container(
-              child: new Image.asset(
-                'assets/images/skydoo_logo_noir.png',
-                scale: 100.0,
-                height: 150.0,
-                fit: BoxFit.cover,
-              ),
-            ),
-            SizedBox(height: 10),
-            //new Text("Let's Quizz", style: new TextStyle(color: Colors.white, fontSize: 50.0, fontWeight: FontWeight.bold),),
-            new Text("Tap to Start the presentation!", style: new TextStyle(color: Colors.white, fontSize: 15.0, fontWeight: FontWeight.bold),),
-            SizedBox(height: 110),
-          ]
-        )
-      ),
-    );
-  }
-}
+// class MainPage extends StatelessWidget {
+//   Widget build (BuildContext context) {
+//     return new Material(
+//       color: Colors.white24,
+//       child: new InkWell(
+//         onTap: () => Navigator.of(context).push(new MaterialPageRoute(builder: (BuildContext context) => ShowPage())),
+//         child: new Column(
+//           mainAxisAlignment: MainAxisAlignment.center,
+//           children: <Widget>[
+//             new Container(
+//               child: new Image.asset(
+//                 'assets/images/skydoo_logo_noir.png',
+//                 scale: 100.0,
+//                 height: 150.0,
+//                 fit: BoxFit.cover,
+//               ),
+//             ),
+//             SizedBox(height: 10),
+//             //new Text("Let's Quizz", style: new TextStyle(color: Colors.white, fontSize: 50.0, fontWeight: FontWeight.bold),),
+//             new Text("Tap to Start the presentation!", style: new TextStyle(color: Colors.white, fontSize: 15.0, fontWeight: FontWeight.bold),),
+//             SizedBox(height: 110),
+//           ]
+//         )
+//       ),
+//     );
+//   }
+// }
