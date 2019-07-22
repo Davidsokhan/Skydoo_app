019e05ba83870927d53261c71af69d716bfd2eab
Overblide <theo.locati@hotmail.fr>
2019-07-22 15:43:52 +0200

    add creation profile page

--- skydoo_communication_app/lib/pages/create_page.dart
+++ skydoo_communication_app/lib/pages/create_page.dart
@@ -1,14 +1,12 @@
 import 'package:flutter/material.dart';
-import './main_page.dart';
-import './create_page.dart';
-import 'pro_menu.dart';
+import 'login_page.dart';
 
-class LoginPage extends StatefulWidget {
+class CreatePage extends StatefulWidget {
   @override
-  LoginPageState createState() => LoginPageState();
+  CreatePageState createState() => CreatePageState();
 }
 
-class LoginPageState extends State<LoginPage> {
+class CreatePageState extends State<CreatePage> {
 
   TextStyle style = TextStyle(fontFamily: 'Montserrat', fontSize: 20.0);
   final emailController = TextEditingController();
@@ -38,27 +36,6 @@ class LoginPageState extends State<LoginPage> {
               OutlineInputBorder(borderRadius: BorderRadius.circular(32.0))),
     );
 
-    final loginButon = Material(
-      elevation: 5.0,
-      borderRadius: BorderRadius.circular(30.0),
-      color: Color(0xff01A0C7),
-      child: MaterialButton(
-        minWidth: MediaQuery.of(context).size.width,
-        padding: EdgeInsets.fromLTRB(20.0, 15.0, 20.0, 15.0),
-        onPressed: () {
-          if (passwordController.text == "admin" && emailController.text == "admin") {
-            Navigator.of(context).pushAndRemoveUntil(new MaterialPageRoute(builder: (BuildContext context) => new ProPage()), (Route route) => route == null);
-          }
-          else {
-          }
-        },
-        child: Text("Login",
-            textAlign: TextAlign.center,
-            style: style.copyWith(
-                color: Colors.white, fontWeight: FontWeight.bold)),
-      ),
-    );
-
     final createButton = Material(
         elevation: 5.0,
         borderRadius: BorderRadius.circular(30.0),
@@ -66,7 +43,7 @@ class LoginPageState extends State<LoginPage> {
         child: MaterialButton(
           minWidth: MediaQuery.of(context).size.width,
           padding: EdgeInsets.fromLTRB(20.0, 15.0, 20.0, 15.0),
-          onPressed: () => Navigator.of(context).pushAndRemoveUntil(new MaterialPageRoute(builder: (BuildContext context) => new CreatePage()), (Route route) => route == null),
+          onPressed: () => Navigator.of(context).pushAndRemoveUntil(new MaterialPageRoute(builder: (BuildContext context) => new LoginPage()), (Route route) => route == null),
           child: Text("Create account",
               textAlign: TextAlign.center,
               style: style.copyWith(
@@ -98,8 +75,6 @@ class LoginPageState extends State<LoginPage> {
                 emailField,
                 SizedBox(height: 25.0),
                 passwordField,
-                SizedBox(height: 35.0),
-                loginButon,
                 SizedBox(height: 35.0),
                 createButton,
                 SizedBox(
